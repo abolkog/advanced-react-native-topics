cf399ae8440840131094d486d31a49d3d50f210e
Khalid Elshafie <abolkog@gmail.com>
2018-08-02 22:17:09 +0900

    Setting Up Redux

--- rendezvous/App.js
+++ rendezvous/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 import Routes from './src/routes';
+import store from './src/store';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Routes />
+      <Provider store={store}>
+        <Routes />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ rendezvous/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import auth from './auth_reducer';
+
+export default combineReducers({
+    auth
+});
