fb1caaa8730961fd12ca543645c247608a26ea64
Khalid Elshafie <abolkog@gmail.com>
2018-08-05 00:45:12 +0900

    Writing First Function

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
-
+const genCode = require('./gen_code');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.greeting = functions.https.onRequest((request, response) => {
+    const body = request.body;
+    const name = request.body.name;
+
+    response.send({
+        greeting: `Hello ${name}`,
+        body: body
+    });
+});
+
+exports.genCode = functions.https.onRequest(genCode);
