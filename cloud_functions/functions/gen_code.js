2c59afd25e1d755c45a5fb9126b9ae8af7ec9b5e
Khalid Elshafie <abolkog@gmail.com>
2018-08-07 00:33:41 +0900

    Sending SMS Function

--- cloud_functions/functions/gen_code.js
+++ cloud_functions/functions/gen_code.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-
+const twilio = require('./twilio_config');
 module.exports = (req, res) => {
     //Check if params (id,phone) exists
     if (!req.body.phone || !req.body.uid) {
@@ -17,11 +17,21 @@ module.exports = (req, res) => {
             const code = Math.floor(Math.random() * 9000 + 1000)
             
             //text via twilio
-
-            //Save code and phone number
-            admin.database().ref(`users/${uid}`).update({ phone,code}).then(() => {
-                return res.send({ success: true });
+            twilio.messages.create({
+                body: `Your Randezvous code is ${code}`,
+                from: '+18604313931',
+                to: `+${phone}`
+            }).then((message) => {
+                //Save code and phone number
+                admin.database().ref(`users/${uid}`).update({ phone, code }).then(() => {
+                    return res.send({ success: true });
+                })
             })
+            .catch((err) => {
+                return res.status(500).send({ error: 'Failed to send SMS', err }) 
+            });
+
+            
         })
         .catch((error) => {
             return res.status(404).send({ error: 'Account not found' }) 
--- rendezvous/src/screens/ConfirmPhoneScreen.js
+++ rendezvous/src/screens/ConfirmPhoneScreen.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FormInput, FormLabel, Button } from 'react-native-elements';
 
 class ConfirmPhoneScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            verifying: false,
+            value: '',
+            loading: false,
+        };
+    }
+
+    getConde() {
+        this.setState({ verifying: true });
+    }
+
+    verifyCode() {
+
+    }
     render() {
+        const { verifying, loading } = this.state;
+        const title = verifying ? 'Enter Code' : 'Enter Phone Number';
+        const btnTxt = verifying ? 'Verify Number' : 'Get Verification Code';
+        const disabled = loading || !this.state.value;
+        const action = verifying ? this.verifyCode : this.getConde;
         return (
             <View style={styles.container}>
-                <Text>ConfirmPhoneScreen</Text>
+                <FormLabel>{title}</FormLabel>
+                <FormInput 
+                    value={this.state.value}
+                    onChangeText={(value) => this.setState({ value })}
+                    keyboardType='phone-pad'
+                />
+                <Button 
+                    title={btnTxt}
+                    containerViewStyle={{ marginTop: 10 }}
+                    disabled={disabled}
+                    loading={loading}
+                    onPress={action.bind(this)}
+                />
             </View>
         );
     }
@@ -13,9 +47,7 @@ class ConfirmPhoneScreen extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flex: 1
     },
 });
 
