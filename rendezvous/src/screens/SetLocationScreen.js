a694e7eb29a99242789023c71c81b9ace3bb0d00
Khalid Elshafie <abolkog@gmail.com>
2018-08-11 20:57:58 +0900

    SetLocationDesign

--- rendezvous/src/screens/SetLocationScreen.js
+++ rendezvous/src/screens/SetLocationScreen.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { View, Dimensions, StyleSheet } from 'react-native';
+import { View, Dimensions, StyleSheet, ScrollView } from 'react-native';
 import { MapView, Location } from 'expo';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+
 import { Spinner } from '../components';
+import Colors from '../constants/Colors';
+
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -17,7 +21,10 @@ class SetLocationScreen extends Component {
         super(props);
         this.state = {
             initMap: true,
-            userLocation: {}
+            userLocation: {},
+            query: '',
+            searchResult: null,
+            btnDisabled: true
         };
     }
 
@@ -28,6 +35,14 @@ class SetLocationScreen extends Component {
         this.setState({ initMap: false, userLocation });
     }
 
+    search() {
+
+    }
+
+    toggleSearchResult() {
+        if (!this.state.searchResult) return;
+    }
+
     render() {
         if (this.state.initMap) {
             return <Spinner />;
@@ -35,24 +50,45 @@ class SetLocationScreen extends Component {
 
         const { userLocation } = this.state;
         return (
-            <View style={styles.container}>
-                <MapView 
-                    style={{ flex: 1 }}
-                    initialRegion={{
-                        latitude: userLocation.latitude,
-                        longitude: userLocation.longitude,
-                        latitudeDelta: LAT_DELTA,
-                        longitudeDelta: LONG_DELTA
-                    }}
+            <ScrollView style={styles.container}>
+                <View style={{ marginBottom: 20 }}>
+                    <FormLabel>Enter Location Name</FormLabel>
+                    <FormInput
+                        onChangeText={(query) => this.setState({ query })}
+                        onSubmitEditing={() => this.search()}
+                    />
+                </View>
+
+                { this.toggleSearchResult() }
+
+                <View style={{ height: 400, marginHorizontal: 15 }} >
+                    <MapView 
+                        style={{ flex: 1 }}
+                        initialRegion={{
+                            latitude: userLocation.latitude,
+                            longitude: userLocation.longitude,
+                            latitudeDelta: LAT_DELTA,
+                            longitudeDelta: LONG_DELTA
+                        }}
+                    />
+                </View>
+
+                <Button 
+                    raised
+                    title='Confirm Location'
+                    containerViewStyle={{ marginTop: 10 }}
+                    buttonStyle={{ backgroundColor: Colors.red }}
+                    disabled={this.state.btnDisabled}
                 />
-            </View>
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: Colors.white
     },
 });
 
